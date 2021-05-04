b239888846a9debda4a98f9d14fd00ddf7e7769a
Avjit Mondal <avijitmondal38@gmail.com>
2021-05-04 20:07:13 +0530

    Added some code on GUI

--- demo/gui/src/App.js
+++ demo/gui/src/App.js
@@ -11,44 +11,62 @@ class App extends Component {
 
     this.state = {
       users: [],
-      appDetails: {}
+      appDetails: {},
+      name: "",
+      id: 0
     };
 
   }
 
   fetchUsers(event) {
-    axios.get('http://localhost:8080/hello?name=avijit').then((response) => {
+    // event.preventDefault();
+    // const data = new FormData(event.target);
+    // var id = data.get('id');
+    // var name = data.get('name');
+    // console.log(id);
+    // console.log(name);
+    // if ((id != null && id >= 10000 && id <= 20000) || (name != null && name.length === 0)) {
+    axios.get('http://localhost:8080/api/users?name=avijit').then((response) => {
       this.setState({ users: response.data })
     })
+    // }
   };
 
   componentDidMount() {
-    axios.get('http://localhost:8080/details').then((response) => {
+    axios.get('http://localhost:8080/api/details').then((response) => {
       this.setState({ appDetails: response.data })
     })
   };
 
-  render() {
-    return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Details Service</p>
-          <p>
-            <li>Hostname : {this.state.appDetails.hostname}</li>
-            <li>IP : {this.state.appDetails.ip}</li>
-            <li>Port : {this.state.appDetails.port}</li>
-          </p>
-          <form onSubmit={this.fetchUsers} >
-            <input name="id" type="number" placeholder="Id" />
-            <input name="name" type="text" placeholder="Name" />
-            <button >Search</button>
-          </form>
-        </header>
-        <Details users={this.state.users} />
-      </div>
-    );
-  }
+  handleNameChange(event) {
+  this.setState({ name: event.target.value });
+  };
+  
+  handleIdChange(event) {
+    this.setState({ id: event.target.value });
+  };
+
+render() {
+  return (
+    <div className="App" >
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Details Service</p>
+        <p>
+          <li>Hostname : {this.state.appDetails.hostname}</li>
+          <li>IP : {this.state.appDetails.ip}</li>
+          <li>Port : {this.state.appDetails.port}</li>
+        </p>
+        <form onSubmit={this.fetchUsers} >
+          <input name="id" type="number" placeholder="Id" value={this.state.id} onChange={this.handleIdChange} />
+          <input name="name" type="text" placeholder="Name" value={this.state.name} onChange={this.handleNameChange} />
+          <button onClick={this.fetchUsers}>Search</button>
+        </form>
+      </header>
+      <Details users={this.state.users} />
+    </div>
+  );
+}
 }
 
 export default App;
